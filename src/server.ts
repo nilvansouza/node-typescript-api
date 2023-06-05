f9f0f533a5ca244bc7841e8530af29042576203c
nillsantana <nillsantana25@gmail.com>
2023-06-05 13:18:21 -0300

    step 4

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+
+export class SetupServer extends Server{
+    
+    constructor(private port = 3000){
+        super();
+    }
+
+    public init(): void {
+        this.setupExpress
+    }
+    
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+    }
+}
